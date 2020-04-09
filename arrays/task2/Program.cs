16208e831807f8bd2b34975c4e27fa436970ec09
Syed Razvi <103219618@swin.edu.au>
2020-04-09 21:45:24 +1000

    task2

---
+++ arrays/task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            int[] numbers = new int[size];
+
+            numbers[0] = 34;
+            numbers[1] = 5;
+            numbers[2] = 67;
+            numbers[3] = 1;
+            numbers[4] = 99;
+            numbers[5] = 34;
+            numbers[6] = 44;
+            numbers[7] = 78;
+            numbers[8] = 34;
+            numbers[9] = 0;
+
+            int index = 0;
+
+            while (index < size)
+            {
+                System.Console.WriteLine(numbers[index]);
+
+                index++;
+            }
+
+            System.Console.WriteLine("------");
+
+            int total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total += numbers[i];
+            }
+            System.Console.WriteLine("= " + total);
+        }
+    }
+}
