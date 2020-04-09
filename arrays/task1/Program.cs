aa02cf57fc903fc9605daa821f86094396a9b998
Syed Razvi <103219618@swin.edu.au>
2020-04-09 20:47:14 +1000

    task1

---
+++ arrays/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[5];
+
+            int i = 0;
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Please Enter Name!");
+                names[i] = Console.ReadLine();
+            }
+            Console.WriteLine("======================");
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Hello " + names[i]);
+            }
+
+        }
+    }
+}
