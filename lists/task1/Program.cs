8162b9aadc7278e2d044962a5c210bdc8026bf5f
Syed Razvi <103219618@swin.edu.au>
2020-04-12 20:14:16 +1000

    task1

--- lists/task1/Program.cs
+++ lists/task1/Program.cs
@@ -6,8 +6,27 @@ namespace task1
     {
         static void Main(string[] args)
         {
-            int size = 5;
-            int[] number = new int[size];
+            List<int> numbers = new List<int>();
+            //add number
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+
+            System.Console.WriteLine("Please Enter a Number:");
+            int usernumber = int.Parse(Console.ReadLine());
+
+            if (numbers.Contains(usernumber))
+            {
+                System.Console.WriteLine("This Number has been previously entered");
+            }
+            else
+            {
+                System.Console.WriteLine("This Number is not in the list");
+            }
+
+            Console.ReadKey();
 
         }
     }
