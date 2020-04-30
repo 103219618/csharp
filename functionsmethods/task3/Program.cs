e35be3d7bc7856205c8bd82c87d402ac94f86c3f
Syed Razvi <103219618@swin.edu.au>
2020-04-30 22:59:23 +1000

    task3

--- functionsmethods/task3/Program.cs
+++ functionsmethods/task3/Program.cs
@@ -4,9 +4,21 @@ namespace task3
 {
     class Program
     {
+        static void sum()
+        {
+            Console.WriteLine("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int add = num1 + num2;
+            
+            Console.WriteLine("The sum of two numbers is: " + add);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            sum();
         }
     }
 }
