9d0a4d1c2a084432ae84c4f51e26baf8efcabe79
Syed Razvi <103219618@swin.edu.au>
2020-04-30 23:24:21 +1000

    task4

--- functionsmethods/task4/Program.cs
+++ functionsmethods/task4/Program.cs
@@ -4,9 +4,24 @@ namespace task4
 {
     class Program
     {
-        static void Main(string[] args)
+        static int SpaceCount(string str)
         {
-            Console.WriteLine("Hello World!");
+            int spaceCo = 0;
+            string space1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                space1 = str.Substring(i, 1);
+                if (space1 == " ")
+                    spaceCo++;
+            }
+            return spaceCo;
+        }
+        static void Main()
+        {
+            string space2;
+            Console.Write("Please input a string: ");
+            space2 = Console.ReadLine();
+            Console.WriteLine("'" + space2 + "'" + " contains " + SpaceCount(space2) + " spaces");
         }
     }
 }
