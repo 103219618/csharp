8093d191551df5fc8be049c922f57ad116e92ddd
Syed Razvi <103219618@swin.edu.au>
2020-05-03 20:14:20 +1000

    task6

---
+++ functionsmethods/task6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task6
+{
+    class Program
+    {
+        static void swap()
+        {
+            int num1;
+            int num2;
+            int newnum;
+
+            Console.Write("Enter a number: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            newnum = num1;
+            num1 = num2;
+            num2 = newnum;
+
+            Console.WriteLine("Now the 1st number is: " + num1 + " and the 2nd number is: " + num2);
+        }
+        static void Main(string[] args)
+        {
+            swap();
+        }
+    }
+}
