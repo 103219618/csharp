75184f079219cc6116db75ba5f2535279bc12d6c
Syed Razvi <103219618@swin.edu.au>
2020-05-03 20:47:02 +1000

    task7

---
+++ functionsmethods/task7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task7
+{
+    class Program
+    {
+        static void toPower()
+        {
+            int baseNum;
+            int powNum;
+            int result = 1;
+
+            Console.WriteLine("Input Base number: ");
+            baseNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the Exponent: ");
+            powNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum;
+            }
+
+            Console.WriteLine("So, the number " + baseNum + " ^(to the power) " + powNum + " = " + result);
+
+        }
+        static void Main(string[] args)
+        {
+            toPower();
+        }
+    }
+}
