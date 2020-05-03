294529ddc5cd1391ceb49701231f388eb705db39
Syed Razvi <103219618@swin.edu.au>
2020-05-03 23:56:48 +1000

    task12opt

---
+++ functionsmethods/task12opt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task12opt
+{
+    class Program
+    {
+        static int fib(int n)
+        {
+            if (n <= 2)
+                return 1;
+
+            else
+                return fib(n - 1) + fib(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The Fibonacci of " + num + "th term is " + fib(num));
+        }
+    }
+}
