95dc5d3eb236210ab544eff76dd037a19c758caa
Syed Razvi <103219618@swin.edu.au>
2020-05-03 23:37:52 +1000

    task11opt

---
+++ functionsmethods/task11opt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task11opt
+{
+    class Program
+    {
+        static decimal factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * factorial(num - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int nmbr;
+            decimal d;
+
+            Console.WriteLine("Enter a number: ");
+            nmbr = int.Parse(Console.ReadLine());
+
+            d = factorial(nmbr);
+            Console.WriteLine("The factorial of " + nmbr + "! is " + d);
+
+        }
+    }
+}
