d755c9b98e53e2ef5f6f9245ab9358380cbdbe74
Syed Razvi <103219618@swin.edu.au>
2020-05-03 22:55:47 +1000

    task9

---
+++ functionsmethods/task9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task9
+{
+    class Program
+    {
+        static bool chkprime(int num)
+        {
+            for (int i = 2; i < num; i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.Write("Input a number: ");
+            n = int.Parse(Console.ReadLine());
+
+            if (chkprime(n))
+            {
+                Console.WriteLine(n + " is a prime number");
+            }
+
+            else
+            {
+                Console.WriteLine(n + " is not a prime number");
+            }
+            Console.WriteLine();
+        }
+    }
+}
