a38d2ef6912640fef71b30f938819fb855030615
Syed Razvi <103219618@swin.edu.au>
2020-05-17 22:25:24 +1000

    task2

---
+++ classesobjects/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    // class person
+    class Person
+    {
+        public string Firstname;
+        public string Surname;
+
+        public int Dob;
+
+        public int height;
+    }
+
+
+}
