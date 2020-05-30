7420085483f5e20a5279de682506438184277963
Syed Razvi <103219618@swin.edu.au>
2020-05-30 20:07:29 +1000

    dice

--- diceroller/Person.cs
+++ diceroller/Person.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace diceroller
 {
-    public class Person
+    public class Person // class is always starts with capital, variable/method will be capitalised when its public
     {
         public string Name;
         public List<Die> Dice;
@@ -26,9 +26,9 @@ namespace diceroller
         public int RollDie(int die)
         {
             int roll = -1; // if die is out of range will return -1
-            if (die < 0 && die > Dice.Count)
+            if (die < 0 && die > this.Dice.Count)
             {
-                roll = Dice[die].Roll();
+                roll = this.Dice[die].Roll();
             }
             return roll; //placeholder
         }
@@ -40,12 +40,27 @@ namespace diceroller
             // access the person's dice list --
             // roll each die -> loop --
             // add roll value to a total
-            
-            int total = 0;
 
+            int total = 0;
+            // can use for loop if know how may iterations
+            // use while loop if dont know how many  iterations
+            /*
+            int count = 0;
+            while (count < this.Dice.Count)
+            {
+                total += Dice[count].Roll();
+                count++;
+            }
+            
             for (var i = 0; i < this.Dice.Count; i++)
             {
                 total += Dice[i].Roll();
+            }*/
+
+            // 6 12 20 sided die
+            // die = 6 sided die
+            foreach(var die in this.Dice) { // the word "die" there can be substituted for anything else, similar to "i"
+                total += die.Roll();
             }
 
             return total; // placeholder
