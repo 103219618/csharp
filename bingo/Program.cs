552decc5330022b5bd4c6687f60577ab0d128d14
Syed Razvi <103219618@swin.edu.au>
2020-06-11 21:44:40 +1000

    selection

--- bingo/Program.cs
+++ bingo/Program.cs
@@ -6,8 +6,12 @@ namespace bingo
     {
         static void Main(string[] args)
         {
-            Game g = new Game("John", "Jane");
-            g.Start();
+            System.Console.WriteLine("Welcome to the Swinburne Bingo Club");
+            System.Console.WriteLine("1. Draw next number");
+            System.Console.WriteLine("2. View all drawn numbers");
+            System.Console.WriteLine("3. Check specific number");
+            System.Console.WriteLine("4. Exit");
+            System.Console.Write("Selection: ");
         }
     }
 }
