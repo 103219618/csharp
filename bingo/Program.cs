5813ab0af4a0c230d6df6f89c638208fdea82c3e
Syed Razvi <103219618@swin.edu.au>
2020-06-13 20:06:44 +1000

    constructor added

--- bingo/Program.cs
+++ bingo/Program.cs
@@ -10,10 +10,10 @@ namespace bingo
             Console.WriteLine("Please enter a number: ");
             int upperNo = Convert.ToInt32(Console.ReadLine());
             RandomGenerator generator = new RandomGenerator();
-            generator.RandomNumber(upperNo);
+            //generator.RandomNumber(upperNo);
 
             //int rand = generator.RandomNumber(1, 100);
-            Console.WriteLine($"Random number is {generator.drawnNo[0]}");
+            
 
             generator.bingoMenu();
 
@@ -31,16 +31,33 @@ namespace bingo
                 int rand;
                 rand = random.Next(1, upperNumber);
                 drawnNo.Add(rand);
+                bingoMenu();
             }
 
             public void bingoMenu()
             {
+                Console.WriteLine(drawnNo.Count);
                 System.Console.WriteLine("Welcome to the Swinburne Bingo Club");
                 System.Console.WriteLine("1. Draw next number");
                 System.Console.WriteLine("2. View all drawn numbers");
                 System.Console.WriteLine("3. Check specific number");
                 System.Console.WriteLine("4. Exit");
                 System.Console.Write("Selection: ");
+
+                var userSelection = Console.ReadLine();
+
+            switch (userSelection)
+            {
+                case "1":
+                    // go to the add menu
+                    RandomNumber(this.upperNumber);
+                    break;
+                
+
+                default:
+                    System.Console.WriteLine("Thank you for playing!");
+                    break;
+            }
 
             }
         }
