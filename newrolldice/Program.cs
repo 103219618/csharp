291217a8702435368162a19e3c4aa0006b4c16ab
Syed Razvi <103219618@swin.edu.au>
2020-06-26 13:36:18 +1000

    checking total

--- newrolldice/Program.cs
+++ newrolldice/Program.cs
@@ -43,6 +43,7 @@ namespace newrolldice
             System.Console.WriteLine("1. Roll Dice");
             System.Console.WriteLine("2. How Many Times Dice Was Rolled");
             System.Console.WriteLine("3. Stop & Check Dice Rolls");
+            System.Console.WriteLine("4. EXIT");
 
             var userSelection = Console.ReadLine();
 
@@ -60,6 +61,11 @@ namespace newrolldice
                     ThirdMenu();
                     break;
 
+                case "4":
+                    System.Console.WriteLine("Thank You For Playing!");
+                    Environment.Exit(0);
+                    break;
+
                 default:
                     System.Console.WriteLine("Incorrect Selection!");
                     Menu();
@@ -70,7 +76,9 @@ namespace newrolldice
         {
             System.Console.WriteLine("1. List in Rolled Order");
             System.Console.WriteLine("2. Order Highest to Lowest");
-            System.Console.WriteLine("3. Main Menu");
+            System.Console.WriteLine("3. Calculate Average");
+            System.Console.WriteLine("4. Calculate Total");
+            System.Console.WriteLine("5. Main Menu");
 
             var userSelection = Console.ReadLine();
 
@@ -85,6 +93,14 @@ namespace newrolldice
                     break;
 
                 case "3":
+                    RollAvg();
+                    break;
+
+                case "4":
+                    RollTotal();
+                    break;
+
+                case "5":
                     Menu();
                     break;
 
@@ -115,11 +131,12 @@ namespace newrolldice
             List<int> lstNew1 = new List<int>();
             lstNew1.AddRange(rollHistory1);
             lstNew1.Sort(); //sorts the numbers
-                            // lstNew2.Reverse(); //reverses the numbers after sorting
+            lstNew1.Reverse(); //reverses the numbers after sorting
 
             List<int> lstNew2 = new List<int>();
             lstNew2.AddRange(rollHistory2);
-            lstNew2.Sort(); //sorts the numbers
+            lstNew2.Sort();  //sorts the numbers
+            lstNew2.Reverse();
 
             System.Console.WriteLine("Dice 1: ");
             foreach (int x in lstNew1)
@@ -132,6 +149,54 @@ namespace newrolldice
             {
                 Console.WriteLine(x);
             }
+            ThirdMenu();
+        }
+        public void RollAvg()
+        {
+            int sum1 = 0;
+            int avg1 = 0;
+            for (int i = 0; i < rollHistory1.Count; i++)
+            {
+                sum1 += rollHistory1[i];
+            }
+
+            avg1 = sum1 / rollHistory1.Count;
+
+            System.Console.WriteLine("Average of Dice 1: " + avg1);
+
+            int sum2 = 0;
+            int avg2 = 0;
+            for (int i = 0; i < rollHistory2.Count; i++)
+            {
+                sum2 += rollHistory2[i];
+            }
+
+            avg2 = sum2 / rollHistory2.Count;
+
+            System.Console.WriteLine("Average of Dice 2: " + avg2);
+
+            System.Console.WriteLine("Combined Average: " + (avg1 + avg2) / 2);
+
+            ThirdMenu();
+        }
+        public void RollTotal()
+        {
+            int sum1 = 0;
+            for (int i = 0; i < rollHistory1.Count; i++)
+            {
+                sum1 += rollHistory1[i];
+            }
+            System.Console.WriteLine("Total of Dice 1: " + sum1);
+
+            int sum2 = 0;
+            for (int i = 0; i < rollHistory2.Count; i++)
+            {
+                sum2 += rollHistory2[i];
+            }
+            System.Console.WriteLine("Total of Dice 2: " + sum2);
+
+            System.Console.WriteLine("Total For Both Dice: " + (sum1 + sum2));
+
             ThirdMenu();
         }
 
