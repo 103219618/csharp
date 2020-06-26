b987b132172f15e5a1146d56ffa30a3bbb44116a
Syed Razvi <103219618@swin.edu.au>
2020-06-26 10:58:52 +1000

    checking random numbers

---
+++ newrolldice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace newrolldice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int roll = rand.Next(1, 6);
+            System.Console.WriteLine(roll);
+            
+        }
+    }
+}
