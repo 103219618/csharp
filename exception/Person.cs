4ae90534c450bc5b14a519b80c8b633483eea762
Syed Razvi <103219618@swin.edu.au>
2020-08-16 22:10:17 +1000

    add person

---
+++ exception/Person.cs
@@ -0,0 +1,18 @@
+namespace exception
+{
+    public class Person
+    {
+        public string Name;
+        public string Room;
+
+        public Person()
+        {
+            this.Name = "";
+        }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
