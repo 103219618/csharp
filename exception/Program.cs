bc8d6c50e4ff6f9dcd61a3d089e492d213ffb273
Syed Razvi <103219618@swin.edu.au>
2020-08-17 12:32:51 +1000

    change

--- exception/Program.cs
+++ exception/Program.cs
@@ -55,7 +55,7 @@ namespace exception
             }
             catch (NullReferenceException ex)
             {
-                System.Console.WriteLine($"Object Doesn't Exist, Closing Program... - {ex.Message}, START AGAIN PLEASE! ");
+                System.Console.WriteLine($"Object Doesn't Exist, Closing Program... - {ex.Message}, START AGAIN PLEASE!");
                 System.Environment.Exit(1);
             }
 
@@ -66,7 +66,7 @@ namespace exception
             }
             catch (NullReferenceException ex)
             {
-                System.Console.WriteLine($"Object Doesn't Exist, Closing Program... - {ex.Message},START AGAIN PLEASE! ");
+                System.Console.WriteLine($"Object Doesn't Exist, Closing Program... - {ex.Message},START AGAIN PLEASE!");
                 System.Environment.Exit(1);
             }
 
