76681e4be10257703812df7ec7774fd262bede24
Syed Razvi <103219618@swin.edu.au>
2020-10-18 00:49:01 +1100

    tests

---
+++ MaskApp/MaskTests/Mask_Tests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using MaskApi.models;
+
+namespace MaskTests
+{
+    public class MaskTest
+    {
+        [Fact]
+        public void ConstructorTest()
+        {
+            try {
+            Mask newMask = new Mask("m-9999", 1, "White", true, false, "flowers", true, 10.00);
+            } catch (Exception ex) {
+                System.Environment.Exit(1);
+            }
+
+            Assert.True(true);
+        }
+    }
+}
---
+++ MaskApp/MaskTests/Order_Tests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using MaskApi.models;
+
+namespace MaskTests
+{
+    public class OrderTest
+    {
+        Mask newMask = new Mask("m-9999", 1, "White", true, false, "flowers", true, 10.00);
+        [Fact]
+        public void SetPriceTest()
+        {
+
+        }
+    }
+}
