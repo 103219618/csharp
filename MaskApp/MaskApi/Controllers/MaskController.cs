cbb3587d8453d98602b6220436fb6cd8e7ce8787
Syed Razvi <103219618@swin.edu.au>
2020-10-18 15:08:09 +1100

    web api controller

---
+++ MaskApp/MaskApi/Controllers/MaskController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaskApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MaskController : ControllerBase
+    {
+        
+    }
+}
