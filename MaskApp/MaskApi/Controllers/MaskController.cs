7df0ec63b6ee0b8e325812c0a1ca58b93605ff2f
Syed Razvi <103219618@swin.edu.au>
2020-10-18 15:27:28 +1100

    added masks

--- MaskApp/MaskApi/Controllers/MaskController.cs
+++ MaskApp/MaskApi/Controllers/MaskController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using MaskApi.models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MaskApi.Controllers
@@ -6,6 +8,13 @@ namespace MaskApi.Controllers
     [Route("[controller]")]
     public class MaskController : ControllerBase
     {
+        public static List<Mask> MasksInStock = new List<Mask>();
         
+        public MaskController() {
+            MasksInStock.Add(new Mask("m-9999", 1, "White", true, false, "Flowers", true, 10.00));
+            MasksInStock.Add(new Mask("m-8888", 3, "Black", false, true, "RaceCars", true, 3.50));
+            MasksInStock.Add(new Mask("m-7777", 1, "Pink", true, false, "Peppa Pig", true, 5.00));
+            MasksInStock.Add(new Mask("m-6666", 3, "Yellow", false, true, "Sponge Bob", true, 5.50));
+        }
     }
 }
